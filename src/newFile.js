c8e0bdbf6b87e32c94a7ea6dad3a7f44adfd81d9
КаролінаКороль19 <karolinakorol421@gmail.com>
2025-06-05 18:21:48 +0300

    doing cod

---
+++ src/newFile.js
@@ -0,0 +1,25 @@
+<script>
+  document.addEventListener('DOMContentLoaded', () => { }
+    const blogPosts = document.querySelectorAll('.blog__post');
+
+    const modal = document.querySelector('.modal');
+    const closeBtn = document.querySelector('.modal__close');
+    const subscribeBtn = document.querySelector('.blog__button');
+
+    if (subscribeBtn) {subscribeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        modal.style.display = 'flex';
+    })};
+    }
+
+    if (closeBtn) {closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    })};
+    }
+
+    window.addEventListener('click', (e) => { }
+    if (e.target === modal) {modal.style.display = 'none'};
+      }
+    });
+  });
+</script>;
